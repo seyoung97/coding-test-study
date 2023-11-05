49c08b1fbb3ec4f3e10964a1a68d2bf9f001f0ed
seyoung97 <seyoung1st@gmail.com>
2023-11-05 22:33:09 +0900

    JavaScript 입출력 문제: A+B

---
+++ 입출력문제풀이.js
@@ -0,0 +1,18 @@
+// A+B
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+//풀이
+// fs 모듈을 이용해 파일 전체를 읽어와 문자열로 저장하기
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+// ["1", "2"]
+let line = input[0].split(" ");
+
+let a = parseInt(line[0]);
+let b = parseInt(line[1]);
+console.log(a + b);
