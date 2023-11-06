373e46aec130fc75a15997d12c35429d2105be81
seyoung97 <seyoung1st@gmail.com>
2023-11-06 12:38:16 +0900

    JavaScript 입출력 문제: 곱셈

--- 입출력문제풀이.js
+++ 입출력문제풀이.js
@@ -63,3 +63,33 @@ console.log(parseInt(e / f));
 // 따라서 몫만 취하기 위해서는 parseInt() 함수를 사용해야함
 // 실수를 정수로 바꾸어 주면 소수점 아래는 버려지는 것 = 몫만 취하는 것
 console.log(e % f);
+
+// 곱셈
+// 문제
+// (세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
+// (1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 (1)의 위치에 들어갈 세 자리 자연수가, 둘째 줄에 (2)의 위치에 들어갈 세자리 자연수가 주어진다.
+
+// 출력
+// 첫째 줄부터 넷째 줄까지 차례대로 (3), (4), (5), (6)에 들어갈 값을 출력한다.
+
+//풀이
+// 핵심 아이디어
+// 세 자리 수 두 개가 주어진다.
+// 문자열로 처리하면, 각 자릿수를 쉽게 얻을 수 있다.
+let fs = require("fs");
+let input4 = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let g = input4[0];
+let h = input4[1];
+
+X_1 = h[2]; // 일의 자리
+X_2 = h[1]; // 십의 자리
+X_3 = h[0]; // 백의 자리
+
+console.log(Number(g) * Number(X_1));
+console.log(Number(g) * Number(X_2));
+console.log(Number(g) * Number(X_3));
+console.log(Number(g) * Number(h));
