39976f93b3a4a6998e70d14bd85a60703d24d07a
seyoung97 <seyoung1st@gmail.com>
2023-11-06 11:52:20 +0900

    JavaScript 입출력 문제: A*B

--- 입출력문제풀이.js
+++ 입출력문제풀이.js
@@ -16,3 +16,23 @@ let line = input[0].split(" ");
 let a = parseInt(line[0]);
 let b = parseInt(line[1]);
 console.log(a + b);
+
+// A*B
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+// 출력
+// 첫째 줄에 A×B를 출력한다.
+
+// 풀이
+let fs = require("fs");
+let input2 = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let line2 = input2[0].split(" ");
+let c = parseInt(line2[0]);
+let d = parseInt(line2[1]);
+
+console.log(c * d);
