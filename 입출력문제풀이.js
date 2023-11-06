fb796a950b862d2fbf6ea6078d10b31149f9a9ca
seyoung97 <seyoung1st@gmail.com>
2023-11-06 12:06:38 +0900

    JavaScript 입출력 문제: 사칙연산

--- 입출력문제풀이.js
+++ 입출력문제풀이.js
@@ -36,3 +36,30 @@ let c = parseInt(line2[0]);
 let d = parseInt(line2[1]);
 
 console.log(c * d);
+
+// 사칙연산
+// 문제
+// 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+
+// 출력
+// 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+
+//풀이
+let fs = require("fs");
+let input3 = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let line3 = input3[0].split(" ");
+let e = Number(line3[0]);
+let f = Number(line3[1]);
+
+console.log(e + f);
+console.log(e - f);
+console.log(e * f);
+console.log(parseInt(e / f));
+// JavaScript에서 나누기 연산(/)을 수행하면 실수 데이터가 반환될 수 있다. 4/8=0.5
+// 따라서 몫만 취하기 위해서는 parseInt() 함수를 사용해야함
+// 실수를 정수로 바꾸어 주면 소수점 아래는 버려지는 것 = 몫만 취하는 것
+console.log(e % f);
