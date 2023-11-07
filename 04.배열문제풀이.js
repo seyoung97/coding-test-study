6fda89cdf4f1e2a534ebc3b39f95c8aa6ff3c323
seyoung97 <seyoung1st@gmail.com>
2023-11-07 19:47:01 +0900

    JavaScript 배열문제: 평균

--- 04.배열문제풀이.js
+++ 04.배열문제풀이.js
@@ -103,3 +103,33 @@ for (let i = 0; i < 10; i++) {
 
 // 집합에 포함된 원소의 개수 출력
 console.log(mySet.size);
+
+// 평균
+// 문제
+// 세준이는 기말고사를 망쳤다. 세준이는 점수를 조작해서 집에 가져가기로 했다.
+// 일단 세준이는 자기 점수 중에 최댓값을 골랐다.이 값을 M이라고 한다.그리고 나서 모든 점수를 점수 / M * 100으로 고쳤다.
+// 예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
+// 세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 시험 본 과목의 개수 N이 주어진다. 이 값은 1000보다 작거나 같다.
+// 둘째 줄에 세준이의 현재 성적이 주어진다.이 값은 100보다 작거나 같은 음이 아닌 정수이고, 적어도 하나의 값은 0보다 크다.
+
+// 풀이
+let fs = require("fs");
+let input4 = fs.readFileSync("/dev/stdin").toString().split("\n");
+let number = Number(input4[0]);
+let data2 = input4[1].split(" ").map(Number);
+
+let maxScore = data2.reduce((a, b) => Math.max(a, b));
+let newData = [];
+
+for (let i = 0; i < number; i++) {
+  newData.push((data2[i] / maxScore) * 100);
+}
+
+let newAverage =
+  newData.reduce((a, b) => {
+    return a + b;
+  }) / number;
+console.log(newAverage);
