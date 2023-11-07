5209308b181050bc14289776871d6a530a00308c
seyoung97 <seyoung1st@gmail.com>
2023-11-07 16:37:26 +0900

    JavaScript 배열문제: 최댓값

--- 04.배열문제풀이.js
+++ 04.배열문제풀이.js
@@ -35,3 +35,41 @@ let minValue2 = data.reduce((a, b) => Math.min(a, b));
 let maxValue2 = data.reduce((a, b) => Math.max(a, b));
 
 console.log(minValue2 + " " + maxValue2);
+
+// 최댓값
+// 문제
+// 9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
+// 예를 들어, 서로 다른 9개의 자연수
+// 3, 29, 38, 12, 57, 74, 40, 85, 61
+// 이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
+
+// 입력
+// 첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
+
+// 핵심 아이디어
+// 배열의 원소를 하나씩 확인하여, 최댓값과 그 인덱스를 찾는 문제
+// 계속해서 가장 큰 갓ㅂ을 가지는 원소의 인덱스를 업데이트한다.
+// 원소를 차례대로 하나씩 확인한단는 점에서 시간 복잡도 O(N)로 해결할 수 있다.
+
+// 풀이
+let fs = require("fs");
+let input2 = fs.readFileSync("/dev/stdin").toString().split("\n");
+let arr2 = input2.map((x) => Number(x));
+let maxValue3 = arr2.reduce((a, b) => Math.max(a, b));
+let maxValueIndex = arr2.indexOf(maxValue3);
+console.log(maxValue3);
+console.log(maxValueIndex + 1);
+
+// 다른 풀이
+let maxIndex = 0;
+let maxValue4 = 0;
+for (let i = 0; i < 9; i++) {
+  let data = Number(input2[i]);
+  if (maxValue < data) {
+    maxValue4 = data;
+    maxIndex = i;
+  }
+}
+
+console.log(maxValue4);
+console.log(maxIndex + 1);
