84356d3c1ec8aa7fa82cec1260fefbbafebafb38
seyoung97 <seyoung1st@gmail.com>
2023-11-10 15:40:14 +0900

    정렬알고리즘: 수정렬하기2

--- 02.수정렬하기.js
+++ 02.수정렬하기.js
@@ -50,3 +50,51 @@ for (let i = 0; i < arr.length; i++) {
   answer += arr[i] + "\n";
 }
 console.log(answer2);
+
+// 수 정렬하기 2
+// 문제
+// N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 수의 개수 N(1<= N <= 1,000,000)이 주어진다.
+// 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다.
+// 수는 중복되지 않는다.
+
+// 출력
+// 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+// 풀이
+// 최대 100만이 넘어가기 때문에 O(N2) 복잡도인 알고리즘(선택, 버블, 삽입)을 사용하면 시간초과
+// 따라서 sort()라이브러리나 병합 알고리즘을 사용해야함
+// sort 사용하면 위랑 똑같이 가능
+
+// 병합 알고리즘 사용한 풀이
+function merge(arr, left, mid, right) {
+  let i = left;
+  let j = mid + 1;
+  let k = left;
+  while (i <= mid && j <= right) {
+    if (arr[i] <= arr[j]) sorted[k++] = arr[i++];
+    else sorted[k++] = arr[j++];
+
+    if (i > mid) {
+      for (; j <= right; j++) sorted[k++] = arr[j];
+    } else {
+      for (; i <= mid; i++) {
+        sorted[k++] = arr[j];
+      }
+    }
+    for (let x = left; x <= right; x++) {
+      arr[x] = sorted[x];
+    }
+  }
+}
+
+function mergeSort(arr, left, right) {
+  if (left < right) {
+    let mid = parseInt((left + right) / 2);
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+  }
+}
