82e78d6201362ee8dc0bc6dbc355cc8d9f274a58
seyoung97 <seyoung1st@gmail.com>
2023-12-11 21:22:23 +0900

    완전탐색: 자릿수의 합

--- 04.완전탐색/자릿수의합.js
+++ 04.완전탐색/자릿수의합.js
@@ -26,3 +26,22 @@ function solution(arr) {
 }
 
 let arr = [128, 460, 603, 40, 521, 137, 123];
+
+// 라이브러리 사용한 풀이
+function solution(arr) {
+  let answer,
+    max = Number.MIN_SAFE_INTEGER;
+  for (let x of arr) {
+    let sum = x
+      .toString()
+      .split("")
+      .reduce((a, b) => a + Number(b), 0);
+    if (sum > max) {
+      max = sum;
+      answer = x;
+    } else if (sum === max) {
+      if (x > answer) answer = x;
+    }
+  }
+  return answer;
+}
